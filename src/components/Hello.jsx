dbbb348caee1c576537d25ef7a033196d95e0da2
kazuki <mugitasoxx@gmail.com>
2021-08-28 18:33:36 +0900

    Sample Create Hello Component

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { bool, shape, string } from 'prop-types';
 
-function Hello() {
+function Hello(props) {
+  const { children, bang, style } = props;
   return (
     <View>
-      <Text>d</Text>
+      <Text style={[styles.text, style]}>
+        {`Thanks ${children}${bang ? '!' : ''}`}
+      </Text>
     </View>
   );
 }
+
+Hello.propTypes = {
+  children: string.isRequired,
+  bang: bool,
+  style: shape(),
+};
+
+Hello.defaultProps = {
+  bang: false,
+  style: null,
+};
+
+const styles = StyleSheet.create({
+  text: {
+    color: '#ffffff',
+    backgroundColor: 'blue',
+    fontSize: 40,
+    fontWeight: 'bold',
+    padding: 16,
+  },
+});
+
+// 他のファイルからHelloを使えるようにする
+export default Hello;

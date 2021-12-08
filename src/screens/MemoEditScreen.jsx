86893649924e950c932e36c4e92ac0721c5320d3
kazuki <mugitasoxx@gmail.com>
2021-12-08 23:21:52 +0900

    add MemoEditScreen,MemoCreateScreen

---
+++ src/screens/MemoEditScreen.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  KeyboardAvoidingView,
+} from 'react-native';
+
+import AppBar from '../components/AppBar';
+import CircleButton from '../components/CircleButton';
+
+// メモ編集画面
+export default function MemoEditScreen() {
+  return (
+    <KeyboardAvoidingView style={Styles.container} behavior="height">
+      <View style={Styles.container}>
+        <AppBar />
+        {/* メモ編集エリア */}
+        <View style={Styles.inputContainer}>
+          <TextInput value="買い物リスト" multiline style={Styles.input} />
+        </View>
+        <CircleButton name="check-decagram" />
+      </View>
+    </KeyboardAvoidingView>
+  );
+}
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  inputContainer: {
+    paddingHorizontal: 27,
+    paddingVertical: 32,
+    flex: 1,
+  },
+  input: {
+    flex: 1,
+    textAlignVertical: 'top',
+    fontSize: 16,
+    lineHeight: 24,
+  },
+});
